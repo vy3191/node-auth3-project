00da00761124f6ac5e8f710c30b80f7c5f123471
vy3191 <venkyafhs@gmail.com>
2020-03-19 22:48:25 -0400

    Completed index.js and server.js file code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const server = express();
+
+server.use(helmet());
+server.use(morgan('tiny'));
+server.use(express.json());
+
+
+server.use((req,res,next) => res.status(404).json({msg:'No Route Found'}));
+server.use((err,req,res,next) => res.status(500).json({msg:'500 Error - Server'}));
+
+module.exports = server;
